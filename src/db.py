41638c9c30bb85f8064f47f9a29f010b71f88204
Aditya Paliwal <aditya@Mac.fritz.box>
2025-01-16 10:54:51 +0100

    bug fix

--- src/db.py
+++ src/db.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 ssm = boto3.client("ssm")
-DB_HOST = "host"  # Replace with your DB endpoint
-DB_USER = "user"  # Replace with your DB user
+DB_HOST = "dshackathongroup1test-rdsinstance-ux1k1fhpkdia.c38uk6c8utvq.eu-central-1.rds.amazonaws.com"  # Replace with your DB endpoint
+DB_USER = "hackathonuser"  # Replace with your DB user
 DB_NAME = "postgres"
 DB_PORT = 5432
 
@@ -19,7 +19,7 @@ def get_db_password():
     """Fetch the DB password securely from SSM"""
     try:
         response = ssm.get_parameter(
-            Name="/team1/rds/password",
+            Name="/hackathonuser/rds/password",
             WithDecryption=True,  # Decrypt the SecureString parameter
         )
         return response["Parameter"]["Value"]
@@ -31,6 +31,7 @@ def get_db_password():
 def connect_to_db():
     """Connect to the PostgreSQL database and return the connection object"""
     db_password = get_db_password()  # Fetch the password securely
+    logger.info(f"DB Password: {db_password}")
 
     if not db_password:
         logger.info("Error: Database password not found.")
